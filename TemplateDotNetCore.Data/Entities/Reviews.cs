e33c096937fe4efb0153d031d568a3c9785cdab1
DESKTOP-BBP39Q0\DELL <ducthinh121994@gmail.com>
2018-01-19 23:34:09 +0700

    Add entites

---
+++ TemplateDotNetCore.Data/Entities/Reviews.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+using TemplateDotNetCore.Infrastucture.SharedKernel;
+
+namespace TemplateDotNetCore.Data.Entities
+{
+    [Table("Reviews")]
+    public class Reviews : DomainEntity<int>
+    {
+        [StringLength(255)]
+        [Required]
+        public string Name { get; set; }
+
+        public string Contents { get; set; }
+
+        [Required]
+        [DefaultValue(0)]
+        public decimal Price { get; set; }
+
+        public string Value { get; set; }
+
+        public double Quantity { get; set; }    
+
+        public int Start { get; set; }
+
+        public bool ShowIndex { get; set; }
+
+    }
+}
---
+++ TemplateDotNetCore.Data/Entities/ShippingInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using TemplateDotNetCore.Infrastucture.SharedKernel;
+
+namespace TemplateDotNetCore.Data.Entities
+{
+    [Table("ShippingInfo")]
+    public class ShippingInfo : DomainEntity<int>
+    {
+        [ForeignKey("BillId")]
+        public virtual Bill Bill { set; get; }
+
+        public int BillId { get; set; }
+
+        [StringLength(255)]
+        [Required]
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        [StringLength(50)]
+        public string Phone { set; get; }
+    }
+}
