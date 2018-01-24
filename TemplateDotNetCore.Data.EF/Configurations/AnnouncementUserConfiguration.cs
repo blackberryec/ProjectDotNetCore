465d9f3a86da01995bd4670c270a4b103e0e1c94
haitran <trannamhaibp@gmail.com>
2018-01-24 23:22:02 +0700

    - Migration Database

---
+++ TemplateDotNetCore.Data.EF/Configurations/AnnouncementUserConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TemplateDotNetCore.Data.EF.Extensions;
+using TemplateDotNetCore.Data.Entities;
+
+namespace TemplateDotNetCore.Data.EF.Configurations
+{
+    public class AnnouncementUserConfiguration : ModelBuilderExtensions.DbEntityConfiguration<AnnouncementUser>
+    {
+        public override void Configure(EntityTypeBuilder<AnnouncementUser> entity)
+        {
+            entity.Property(c => c.Id).HasMaxLength(128).IsRequired();
+        }
+    }
+}
