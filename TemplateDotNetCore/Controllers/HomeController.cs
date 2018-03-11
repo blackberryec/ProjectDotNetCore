4be1fb01eb4f02674131c61f31318aabc3827b31
haitran <trannamhaibp@gmail.com>
2018-03-11 15:01:29 +0700

    bug layout

--- TemplateDotNetCore/Controllers/HomeController.cs
+++ TemplateDotNetCore/Controllers/HomeController.cs
@@ -14,10 +14,5 @@ namespace TemplateDotNetCore.Controllers
         {
             return View();
         }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
--- TemplateDotNetCore/Startup.cs
+++ TemplateDotNetCore/Startup.cs
@@ -111,9 +111,11 @@ namespace TemplateDotNetCore
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
                 routes.MapRoute(
                     name: "areaRoute",
                     template: "{area:exists}/{controller=Login}/{action=Index}/{id?}");
+
             });
         }
     }
