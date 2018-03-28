a12b08e567bb5b49409add01667bf7679b650e17
haitran <trannamhaibp@gmail.com>
2018-03-29 00:01:24 +0700

    make cart component

---
+++ TemplateDotNetCore/Controllers/Components/HeaderCartViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TemplateDotNetCore.Controllers.Components
+{
+    public class HeaderCartViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
